f4d6e31599c47335505ea793805ccdaaa880bd80
Rabuni Xavier <rabuni.xavier@zucisystems.com>
2022-09-01 13:15:50 +0530

    initialcommit

---
+++ XML-Project/DropDownListApplication/DropDownListApplication/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace DropDownListApplication
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+            StyleBundle(bundles);
+            ScriptBundle(bundles);
+        }
+        public static void StyleBundle(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css")
+                     .Include("~/Content/bootstrap.css"));
+        }
+        public static void ScriptBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js")
+                     .Include("~/Scripts/jquery-{version}.js")
+                     .Include("~/Scripts/bootstrap.js"));
+        }
+    }
+}
---
+++ XML-Project/DropDownListApplication/DropDownListApplication/Controllers/GridListController.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+
+
+namespace DropDownListApplication.Controllers
+{
+    public class GridListController : Controller
+    {
+        [HttpGet]
+        public ActionResult GridView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GridView(HttpPostedFileBase GetXmlFile, string store)
+        {
+            string path = Server.MapPath("~/XmlFile/");
+            String[] allowExtention = { ".xml" };
+            string extension = Path.GetExtension(GetXmlFile.FileName);
+            if (extension == allowExtention[0])
+            {
+                try
+                {
+                    var xml = XDocument.Load(Server.MapPath("~/XmlFile/Dummy.xml"));
+                    Dictionary<string, string> nodelist = new Dictionary<string, string>();
+                    nodelist.Add("Form", "Form");
+                    foreach (var element in xml.Root.Elements())
+                    {
+                        nodelist.Add(element.Name.ToString(), element.Name.ToString());
+
+                    }
+                    Dictionary<string, string> node2list = new Dictionary<string, string>();
+                    foreach (var firstparent in nodelist)
+                    {
+                        foreach (var element in xml.Root.Descendants(firstparent.Key))
+                        {
+                            node2list.Add(firstparent.Key, element.Name.ToString());
+
+                        }
+                    }
+                    Dictionary<string, string> node3list = new Dictionary<string, string>();
+                    foreach (var firstparent in nodelist)
+                    {
+                        foreach (var secondparent in node2list)
+                        {
+                            foreach (var element in xml.Root.Elements(secondparent.Key))
+                            {
+                                node3list.Add(secondparent.Key, element.Name.ToString());
+                            }
+                        }
+                    }
+
+                    Dictionary<string, string> node4list = new Dictionary<string, string>();
+                    foreach (var firstparent in nodelist)
+                    {
+                        foreach (var secondparent in node2list)
+                        {
+                            foreach (var thirdparent in node3list)
+                            {
+                                foreach (var element in xml.Root.Elements(thirdparent.Key))
+                                {
+                                    node4list.Add(thirdparent.Key, element.Name.ToString());
+                                }
+                            }
+                        }
+                    }
+
+
+                    GetXmlFile.SaveAs(path + GetXmlFile.FileName);
+                    ViewBag.ErrorMessage = "File  uploaded Successfully!!";
+                    store = path + GetXmlFile.FileName;
+                    GetXmlFile.SaveAs(path + GetXmlFile.FileName);
+                }
+                catch
+                {
+                    ViewBag.ErrorMessage = "File Cannot be uploaded";
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "File not allow to upload";
+            }
+            return View();
+
+
+
+        }
+    }
+}
