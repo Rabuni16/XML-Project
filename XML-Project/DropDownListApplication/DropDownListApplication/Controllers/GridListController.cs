19a200de581f1b8f25093cde80b117ad3474e8da
Rabuni Xavier <rabuni.xavier@zucisystems.com>
2022-09-09 17:01:59 +0530

    updated the UI
    updated the UI

--- XML-Project/DropDownListApplication/DropDownListApplication/Controllers/GridListController.cs
+++ XML-Project/DropDownListApplication/DropDownListApplication/Controllers/GridListController.cs
@@ -5,21 +5,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Script.Serialization;
 using System.Xml;
-
-
+using ClosedXML.Excel;
+using System.Data;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.Graph;
+using Microsoft.Office.Interop.Excel;
+using Worksheet = Microsoft.Office.Interop.Excel.Worksheet;
+using Workbook = DocumentFormat.OpenXml.Spreadsheet.Workbook;
+using System.Configuration;
+using System.Linq;
 
 namespace DropDownListApplication.Controllers
 {
     public class GridListController : Controller
     {
-       /* private readonly IConfiguration _configuration;
-
-        public GridListController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }*/
 
 
 
@@ -29,130 +29,186 @@ namespace DropDownListApplication.Controllers
         [HttpGet]
         public ActionResult GridView()
         {
+
             return View();
         }
 
-        [HttpPost]
-        public ActionResult GridView(HttpPostedFileBase ImageFile)
+        public ActionResult Export(string value, string key)
         {
-            
+            /*i = 0;
+            var walue[i] = value;
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+
+            app.Visible = true;
+
+
+            Worksheet = Workbook.Sheets["Sheet1"];
+            worksheet = Workbook.ActiveSheet;
+
+            worksheet.Name = "Exported from gridview";
+
+            for (int i = 1; i < value[].Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+
+            workbook.SaveAs("c:\\output.xls", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            app.Quit();*/
+            /*DataTable products = new DataTable("Grid");
+            products.Columns.Add("Key", typeof(string));
+            products.Columns.Add("value", typeof(string));
+
+            products.Rows.Add(key, value);
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                wb.Worksheets.Add(products);
+                using (MemoryStream stream = new MemoryStream())  
+                {
+                    *//*wb.SaveAs(stream);*//*
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    return File(stream.ToString(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Report.xlsx");
+                }
+
+            }
+            return RedirectToAction("GridView");*/
             return View();
-        }
-
-        //Get file into select the path
-
-        [HttpPost]
-        public ActionResult GetFile()
-        {
-            if (Request.Files.Count > 0)
-            {
-                try
-                {
-                   
-                    HttpFileCollectionBase files = Request.Files;
-                    XmlNodeList firstNodelist=null;
-                    for (int i = 0; i < files.Count; i++)
-                    {
-                       
-
-                        HttpPostedFileBase file = files[i];
-                        string fname;
-
-                        
-                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
-                        {
-                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
-                            fname = testfiles[testfiles.Length - 1];
-                        }
-                        else
-                        {
-                            fname = file.FileName;
-                        }
-                        
-                        
-                        fname = Path.Combine(Server.MapPath("~/XmlFile/"), fname);
-                        file.SaveAs(fname);
-                        
-                    }
-
-                    
-                    return Json(NodeObject(), JsonRequestBehavior.AllowGet);
-                }
-                catch (Exception ex)
-                {
-                    return Json("Error occurred. Error details: " + ex.Message);
-                }
-            }
-            else
-            {
-                return Json("No files selected.");
-            }
         }
 
         //send the dropdownlists values 
+        string keyvalue = System.Configuration.ConfigurationManager.AppSettings["XmlReaderNode"];
+
         public ActionResult NodeObject()
         {
 
-            XmlDataDocument xmldoc = new XmlDataDocument();
-            string filename = System.Web.Hosting.HostingEnvironment.MapPath("~/XmlFile/Dummy.Xml");
-            List<NodeObject> Nodelist = new List<NodeObject>();
-            try
+            if (Request.Files.Count > 0)
             {
-                bool haschildnode = false;
-                string str = null;
-                FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
-                xmldoc.Load(fs);
-                haschildnode = xmldoc.HasChildNodes;
-                if (haschildnode)
+                HttpFileCollectionBase files = Request.Files;
+                HttpPostedFileBase file = files[0];
+                try
                 {
-                    XmlElement elm = xmldoc.DocumentElement;
-                    XmlNodeList nodelist = elm.ChildNodes;
+                    string fname;
+                    fname = file.FileName;
+                    string extension = Path.GetExtension(file.FileName);
 
+                    string[] allowExtention = { ".xml" };
 
-                    for (int i = 0; i <= nodelist.Count - 1; i++)
+                    if (extension == allowExtention[0])
                     {
-                        NodeObject oNodelist = new NodeObject();
-                        str = nodelist[i].Name;
-                        oNodelist.code = str;
-                        string content = nodelist[i].FirstChild == null ? null : nodelist[i].FirstChild.ToString();
-                        if (content != "System.Xml.XmlText" || content == null)
+                        fname = Path.Combine(Server.MapPath("~/Uploads/"), fname);
+                        file.SaveAs(fname);
+                        XmlDataDocument xmldoc = new XmlDataDocument();
+                        List<NodeObject> Nodelist = new List<NodeObject>();
+                        
+                        using (FileStream fs = new FileStream(fname, FileMode.Open, FileAccess.Read))
                         {
-                            oNodelist.value = null;
-                        }
-                        else
-                        {
-                            oNodelist.value = nodelist[i].InnerText;
-                        }
-                        Nodelist.Add(oNodelist);
-                    }
-                    return Json(Nodelist, JsonRequestBehavior.AllowGet);
-                }
-            }
-            catch (Exception)
-            {
+                            xmldoc.Load(fs);
+                            bool haschildnode = false;
+                            string str = null;
+                            haschildnode = xmldoc.HasChildNodes;
+                            if (haschildnode)
+                            {
+                                XmlElement elm = xmldoc.DocumentElement;
+                                XmlNodeList nodelist = elm.ChildNodes;
+                                for (int j = 0; j <= nodelist.Count - 1; j++)
+                                {
 
+                                    NodeObject oNodelist = new NodeObject();
+                                    str = nodelist[j].Name;
+                                    oNodelist.code = str;
+                                    string content = nodelist[j].FirstChild == null ? null : nodelist[j].FirstChild.ToString();
+                                    if (content != "System.Xml.XmlText" || content == null)
+                                    {
+                                        oNodelist.value = null;
+                                    }
+                                    else
+                                    {
+                                        oNodelist.value = nodelist[j].InnerText;
+                                    }
+                                    Nodelist.Add(oNodelist);
+                                }
+                                List<NodeObject> nodeListNew= new List<NodeObject>();
+                                List<string> keylist = new List<string>();
+                                if (!string.IsNullOrEmpty(keyvalue))
+                                {
+
+                                    string[] keyvaluelist = keyvalue.Split(new char[] {','});
+                                    if (keyvaluelist.Length > 0)
+                                    {
+                                        
+                                        foreach(var key in keyvaluelist)
+                                        {
+                                            keylist.Add(key.Trim());
+                                        }
+                                    }
+                                }
+                                if (nodelist != null&& keylist != null)
+                                {
+                                    foreach(var key in Nodelist)
+                                    {
+                                        if (keylist.Contains(key.code))
+                                        {
+                                            nodeListNew.Add(key);
+                                        }
+                                    }
+                                    Nodelist = nodeListNew;
+                                }
+
+
+                            }
+                           
+                            return Json( Nodelist, JsonRequestBehavior.AllowGet);
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+
+                }
             }
             return null;
         }
 
-        
-        
 
-       //Read the xml value and send first dropdownlist 
-        public ActionResult GetFirstChileNodeByName(string tagName)
+        //Read the xml value and send first dropdownlist 
+        public ActionResult GetFirstChileNodeByName(string tagName, string uploadFileName)
         {
             string firstNode = tagName;
-            XmlNodeList store =GetXmlNodeList(firstNode);
+
             List<NodeObject> cboSecondNode = new List<NodeObject>();
+
+
+            XmlNodeList store = GetXmlNodeList(firstNode, uploadFileName);
             for (int i = 0; i <= store.Count - 1; i++)
             {
                 NodeObject oNodelist = new NodeObject();
                 string str = store[i].Name;
                 oNodelist.code = str;
-                string content = store[i].FirstChild==null?null:store[i].FirstChild.ToString();
-                if(content != "System.Xml.XmlText" || content==null)
+                string content = store[i].FirstChild == null ? null : store[i].FirstChild.ToString();
+                if (content != "System.Xml.XmlText" || content == null)
                 {
-                    oNodelist.value = null;                                   
+                    oNodelist.value = null;
                 }
                 else
                 {
@@ -164,27 +220,33 @@ namespace DropDownListApplication.Controllers
         }
 
         //Read NodeList to send the dropdownlist
-        public XmlNodeList GetXmlNodeList(string getNode)
+        public XmlNodeList GetXmlNodeList(string getNode, string UploadedFileName)
         {
             XmlDataDocument xmldoc = new XmlDataDocument();
-            XmlNodeList nodelist = null;
-            string filename = System.Web.Hosting.HostingEnvironment.MapPath("~/XmlFile/Dummy.Xml");
-            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
-            xmldoc.Load(fs);
-            /*XmlNodeList xmlNodebyname = xmldoc.SelectNodes("//" + getNode);*/
-            XmlNodeList xmlNodebyname = xmldoc.GetElementsByTagName(getNode);
-
-            for (int i = 0; i < xmlNodebyname.Count; i++)
+            XmlNodeList nodelist = null;            string filename = System.Web.Hosting.HostingEnvironment.MapPath("~/Uploads/" + UploadedFileName);
+            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
             {
-                nodelist = xmlNodebyname[i].ChildNodes;
-                
+                xmldoc.Load(fs);
+                bool haschildnode = false;
+                haschildnode = xmldoc.HasChildNodes;
+                if (haschildnode)
+                {
+                    string getValue = xmldoc.ChildNodes[1].Name;
+                    var firstRoot = "//" + getValue + getNode;
+                    XmlNodeList xmlNodebyname = xmldoc.SelectNodes(firstRoot);
+
+                    for (int i = 0; i < xmlNodebyname.Count; i++)
+                    {
+                        nodelist = xmlNodebyname[i].ChildNodes;
+                    }
+                    return nodelist;
+
+                }
             }
-            return nodelist;
+
+            return null;
         }
-
-        
-       
     }
-
-
 }
+
+
---
+++ XML-Project/DropDownListApplication/DropDownListApplication/Models/ExportExcel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DropDownListApplication.Models
+{
+    public class ExportExcel
+    {
+        public string value  { get; set; }
+        public string Key { get; set; }
+    }
+}
